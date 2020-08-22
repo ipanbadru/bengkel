98230f9faba6104016695da75792e778140d6d96
ipanbadru <ipanbzn1@gmail.com>
2020-08-22 09:42:59 +0700

    Merubah Tampilan Dashboard

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -22,12 +22,20 @@ class Admin extends BaseController
         foreach ($tanggal as $tgl) {
             $jumlah_transaksi[] = $transaksi->where('tanggal', $tgl)->countAllResults();
         }
+        foreach ($barang->findAll() as $b) {
+            $nama_barang[] = $b['barang'];
+        }
+        foreach ($nama_barang as $b) {
+            $jumlah_barang[] = $barang->where('barang', $b)->first()['stok'];
+        }
         $data =
             [
                 'active' => 'dashboard',
                 'title' => 'Dashboard',
                 'transaksi' => $transaksi->findAll(),
                 'jumlah_transaksi_perhari' => $jumlah_transaksi,
+                'nama_barang' => $nama_barang,
+                'jumlah_barang' => $jumlah_barang,
                 'tanggal' => array_unique($tanggal),
                 'jumlah_pelanggan' => $pelanggan->countAll(),
                 'jumlah_stok' => $barang->selectSum('stok')->first(),
